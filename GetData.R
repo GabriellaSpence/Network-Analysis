b96fa4b9c1ea8ca01e2164cadf701c5d4cbb08a4
gxespence <gxespence@gmail.com>
2022-07-02 09:57:20 -0700

    Initial Commit

---
+++ GetData.R
@@ -0,0 +1,16 @@
+library(rtweet)
+
+## authenticate via web browser
+token <- create_token(
+  app = "rstatsjournalismresearch",
+  consumer_key = Sys.getenv("key"),
+  consumer_secret = Sys.getenv("secret"))
+
+get_token()
+
+# search tweets
+rt <- search_tweets("#peopleanalytics", n = 500, include_rts = TRUE)
+View(rt)
+
+#Save data for later
+save(rt, file = "Data.RData")
