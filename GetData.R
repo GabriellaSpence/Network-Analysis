20432d973478214fd7946a135b09369c2b6874e1
gxespence <gxespence@gmail.com>
2022-07-03 18:20:38 -0700

    Update GetData

--- GetData.R
+++ GetData.R
@@ -1,12 +1,12 @@
 library(rtweet)
 
-## authenticate via web browser
-token <- create_token(
-  app = "rstatsjournalismresearch",
-  consumer_key = Sys.getenv("key"),
-  consumer_secret = Sys.getenv("secret"))
-
-get_token()
+# ## authenticate via web browser
+# token <- create_token(
+#   app = "gxs",
+#   consumer_key = Sys.getenv("key"),
+#   consumer_secret = Sys.getenv("secret"))
+# 
+# get_token()
 
 # search tweets
 rt <- search_tweets("#peopleanalytics", n = 500, include_rts = TRUE, lang = "en")
